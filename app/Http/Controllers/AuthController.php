809cc4223b265d1b17c9438d45fa493bc1f226e3
Sergey <52923458+Sergey98Am@users.noreply.github.com>
2020-09-16 12:02:10 +0400

    test commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,7 @@ class AuthController extends Controller
     public function __construct()
     {
         $this->middleware('jwt', ['except' => ['login']]);
+        // TODO:test
     }
 
     /**
