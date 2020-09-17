17750e18bae40d7ca7762d41e26ec1b19f51c892
Sergey <52923458+Sergey98Am@users.noreply.github.com>
2020-09-17 10:50:47 +0400

    JWT Token (Updated)

--- public/js/script.js
+++ public/js/script.js
@@ -14,16 +14,11 @@ $(document).ready(function () {
     $('.login').on('submit', function (e) {
         e.preventDefault()
         var form = $(this);
-        $.ajaxSetup({
-            headers: {
-                'jwt': $('meta[name="jwt"]').attr('content')
-            }
-        });
         $.ajax({
-            url: form.attr('action'),
+            url: 'http://carshop.loc/api/auth/login',
             method: 'post',
             data: form.serialize(),
-            headers: {"Authorization": localStorage.getItem('2GT9Kzm6dBNtJGB9Y3Z7tXo4Epa8aNqIOLLL89e9ZM5IExLDEGHTiS4v0cS0ryAx')},
+            headers: {"token": localStorage.getItem('2GT9Kzm6dBNtJGB9Y3Z7tXo4Epa8aNqIOLLL89e9ZM5IExLDEGHTiS4v0cS0ryAx')},
             success: function () {
 
             
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="jwt" content="2GT9Kzm6dBNtJGB9Y3Z7tXo4Epa8aNqIOLLL89e9ZM5IExLDEGHTiS4v0cS0ryAx">
-
 
     <title>Project</title>
 
